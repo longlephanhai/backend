12995c33cf5edc8ae3af0b7318c7b59535037d5d
Lê Phan Hải Long <longlph.23it@gmail.com>
2024-06-25 15:54:46 +0700

    update

--- index.js
+++ index.js
@@ -16,7 +16,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use("/api", router)
 const PORT = 8080 || process.env.PORT
-app.send("backend")
+app.get("/", (req, res) => {
+    res.send("API Working");
+});
+
 connectDB().then(() => {
     app.listen(PORT, () => {
         console.log("connect to DB")
